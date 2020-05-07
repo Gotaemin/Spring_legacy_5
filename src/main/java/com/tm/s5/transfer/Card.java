018bdc3b792ff51bbfab86934beb4b772b80d7f8
Gotaemin <gtm1213@naver.com>
2020-05-07 12:38:32 +0900

    annotaion

--- src/main/java/com/tm/s5/transfer/Card.java
+++ src/main/java/com/tm/s5/transfer/Card.java
@@ -1,14 +1,22 @@
 package com.tm.s5.transfer;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class Card {
 	
+	@AfterReturning("execution(* com.tm.s5.transfer.Transfer.*())")
 	public void cardAfter() {
 		System.out.println("=============================");
 		System.out.println("카드 결제");
 	}
 	
+	@Around("execution(* com.tm.s5.transfer.Transfer.*(Integer,..))")
 	//around를 사용하면 ProceedingJoinPoint필요
 	public Object cardCheck(ProceedingJoinPoint joinPoint) throws Throwable {
 		System.out.println("=============================");
